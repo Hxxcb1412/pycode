553bb7f232c99546a98a46e428dcfe21a67c26a7
Hxxcb1412 <97016347+Hxxcb1412@users.noreply.github.com>
2023-01-24 01:21:02 +0800

    add
    add

---
+++ 02-变量.py
@@ -0,0 +1,15 @@
+"""
+1.定义变量
+    语法：变量名 = 值
+
+2.使用变量
+3.看变量的特点
+"""
+
+#   定义变量：存储数据TOM
+my_name = 'TOM'
+print(my_name)
+
+#   定义变量：存储数据 Shido
+newName = 'i am Shido, i love Python'
+print(newName)
---
+++ 03-认识bug.py
@@ -0,0 +1,8 @@
+
+#   定义变量：存储数据TOM
+my_name = 'TOM'
+print(my_name)
+
+#   定义变量：存储数据 Shido
+newName = 'i am Shido, i love Python'
+print(newName)
